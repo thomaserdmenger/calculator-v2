e1db768b39c213f892ae7deeec7ebf31f00181e8
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-07-17 06:30:01 +0200

    Render first number to screen

--- script.js
+++ script.js
@@ -10,4 +10,22 @@ const deleteButton = document.querySelector('[data-delete]')
 const percentageButton = document.querySelector('[data-percentage]')
 const equalsButton = document.querySelector('[data-equals]')
 
-const content = document.querySelector('[data-content]')
+const display = document.querySelector('[data-content]')
+
+// add event listener on numbers
+numbersArray.forEach(number => number.addEventListener('click', handleNumber))
+
+function handleNumber(e) {
+  // put first number to operate function
+  let firstNumber = display.textContent
+  let secondNumber = e.target.textContent
+
+  console.log(firstNumber)
+  console.log(secondNumber)
+
+  if (display.textContent === '0') {
+    display.textContent = secondNumber
+  } else {
+    display.textContent += secondNumber
+  }
+}
